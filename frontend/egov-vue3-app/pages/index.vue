e31008c3560894f70b97d40c8e91a85e23644c94
chaelin01211 <chaelin01211@gmail.com>
2023-06-26 14:17:41 +0900

    페이지 이동 방식 변경 -> 전역 상태 유지 (reload 방지)

--- frontend/egov-vue3-app/pages/index.vue
+++ frontend/egov-vue3-app/pages/index.vue
@@ -1,11 +1,16 @@
 <template>
   <Home :title="'Home Title'" />
-  <a href="/sub">router link 테스트</a>
+  <button @click="navigateToPage">페이지로 이동</button>
+  <!--  아래와 동일하게 작동 -->
+  <!--  <button @click="router.push('/sub')">페이지로 이동</button>-->
 </template>
 
-<script>
-export default {
-  name: "index"
+<script setup>
+const router = useRouter();
+
+const navigateToPage = () => {
+  // 페이지 이동 로직 작성
+  router.push("/sub"); // 이동할 페이지 경로를 지정합니다.
 };
 </script>
 
--- frontend/egov-vue3-app/pages/sub/index.vue
+++ frontend/egov-vue3-app/pages/sub/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div>router test page</div>
+  <p>전역 상태 유지 - Pinia</p>
+  <p>{{ count }}</p>
+  <p>{{ doubleCount }}</p>
 </template>
 
-<script>
-export default {
-  name: "index"
-};
+<script setup>
+const counterState = useCounterStore();
+const { count, doubleCount, increment } = counterState;
 </script>
 
 <style scoped></style>
